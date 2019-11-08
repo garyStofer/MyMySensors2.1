c9f29d0e10cc6fe80ae91b2f9fbd76a0ccf4ccea
Gary Stofer <garystofer@gmail.com>
2019-11-08 11:25:59 -0800

    added references to X10 protocol documentation fixed X10 weird bit assignment for device numbers expanded number of X10 lights to be controlled to 12

--- myGatewaySerial_X10/myGatewaySerial_X10.ino
+++ myGatewaySerial_X10/myGatewaySerial_X10.ino
@@ -89,8 +89,8 @@
 #define MyX10_HOUSE       0 // House code  A -- hardcoded for now
 #define X10_PIN           3 // modulation on/off keying pin for 310Mhz transmitter
 // bit times as per NEC IR remote control protocol
-#define NEC_HDR_MARK    (16*NEC_BIT_MARK)
 #define NEC_HDR_SPACE   (8*NEC_BIT_MARK)
+#define NEC_HDR_MARK    (16*NEC_BIT_MARK)
 #define NEC_BIT_MARK     560
 #define NEC_RPT_SPACE   2250
 #define MARK 1
@@ -104,16 +104,20 @@ void before()
 // Modified usec delay for X10 transmission
 void MyDelay_usec(unsigned long uSecs) 
 {
+// note: micros() might skipp a count if interrupts get in between so we can't just wait for == endcount
+// on 16mhz clocked system, micros increments in 4 us steps.
   if (uSecs > 4) 
   {
-    unsigned long start = micros();
-    unsigned long endMicros = start + uSecs - 4;
-    
-    if (endMicros < start) 
-    { // Check if overflow
-      while ( micros() > start ) {} // wait until overflow
-    }
-    while ( micros() < endMicros ) {} // normal wait
+    unsigned long now = micros();
+    unsigned long end = now + uSecs-4;
+	
+	if (end < now)	// wrapped around
+	{
+		while ( micros() >= end ) {} //  wait until wrap around
+	}
+   	
+	while ( micros() <= end ) {} //  wait until it hits end 
+
  } 
 }
 
@@ -129,13 +133,34 @@ void X10_out ( bool mark, unsigned int time)
 // The lookup table for house codes as they are not sequential.
 const static byte HouseCode[] ={6,14,2,10,1,9,5,13,7,15,3,11,0,8,4,12};  // A=0, B=1,....
 
-// Transmits a complete b32 bit burst according to NEC IR control protocl
+//Device numbers do not map directly to bits in the command word. Command bit 0 Indicates Dim/Bright, bit 2 controls On/OFF
+// Device# bit, (weight) :  Commnad byte bit, (weight)
+//		0,(0x1)	: 	3,(0x8)
+// 		1,(0x2)	:	4,(0x10)
+//		2,(0x4)	:	1,(0x2)
+//		3,(0x8)	:	5,(0x20)
+
+  
+
+
+// Transmits a complete 32 bit burst according to NEC IR control protocl
 void X10_transmit( byte house, byte device_no, bool ON_off)
 {
   house = HouseCode[house&0xf]; // Looks up sequential house numbers to the nonsequential codes used by X10
+  device_no &=0xf;				//max of 16 devices
   
   unsigned short housebits = house |(~house) << 8;
-  unsigned short device =  (device_no << 3) |  (ON_off ? 0:4);  // lower 3 bits are control code followed by device number
+  
+  unsigned short device =  ((device_no << 3) &0x18); 	// get bits 3 and 4 in place 
+  
+  if (device_no & 0x4 )		// fill in out of order device bit 2
+	device |= 0x2;
+		
+  if (device_no & 0x8 )		// fill in out of order device bit 3
+	device |= 0x20;
+    
+  device |=  (ON_off ? 0:0x4);  // Fill in the On/Off bit
+  
   unsigned short devicebits = device | ((~device) << 8UL);
   unsigned long X10_bits = housebits | ((unsigned long)devicebits << 16UL); 
 
@@ -172,16 +197,20 @@ void setup()
 
 void presentation()
 {
-	// Present 8 locally attached X10 actuators
-	sendSketchInfo("mySensorsGatewayWithX10", "2.11");
-	present(CHILD_X10_1, S_BINARY);
-	present(CHILD_X10_1+1, S_BINARY);
-	present(CHILD_X10_1+2, S_BINARY);
-	present(CHILD_X10_1+3, S_BINARY);
+	// Present 12 locally attached X10 actuators -- max is 16
+  sendSketchInfo("mySensorsGatewayWithX10", "2.11");
+  present(CHILD_X10_1, S_BINARY);
+  present(CHILD_X10_1+1, S_BINARY);
+  present(CHILD_X10_1+2, S_BINARY);
+  present(CHILD_X10_1+3, S_BINARY);
   present(CHILD_X10_1+4, S_BINARY);
   present(CHILD_X10_1+5, S_BINARY);
   present(CHILD_X10_1+6, S_BINARY);
   present(CHILD_X10_1+7, S_BINARY);
+  present(CHILD_X10_1+8, S_BINARY);
+  present(CHILD_X10_1+9, S_BINARY);
+  present(CHILD_X10_1+10, S_BINARY);
+  present(CHILD_X10_1+11, S_BINARY);
 
 }
 void receive(const MyMessage &message)
@@ -190,11 +219,11 @@ void receive(const MyMessage &message)
 	if (message.type==V_STATUS) 
 	{
 		// Change relay state
-	 if (message.sensor == CHILD_X10_1) 
-         X10_transmit( MyX10_HOUSE, 0, message.getBool());
+		if (message.sensor == CHILD_X10_1) 
+			X10_transmit( MyX10_HOUSE, 0, message.getBool());
          
 		if (message.sensor == CHILD_X10_1+1) 
-         X10_transmit( MyX10_HOUSE, 1, message.getBool());
+			X10_transmit( MyX10_HOUSE, 1, message.getBool());
    
 		if (message.sensor == CHILD_X10_1+2) 
 			 X10_transmit( MyX10_HOUSE, 2, message.getBool());
@@ -202,17 +231,29 @@ void receive(const MyMessage &message)
 		if (message.sensor == CHILD_X10_1+3) 
 			 X10_transmit( MyX10_HOUSE, 3, message.getBool());
 
-       if (message.sensor == CHILD_X10_1+4) 
-         X10_transmit( MyX10_HOUSE, 0, message.getBool());
+		if (message.sensor == CHILD_X10_1+4) 
+			X10_transmit( MyX10_HOUSE, 4, message.getBool());
          
-   if (message.sensor == CHILD_X10_1+5) 
-         X10_transmit( MyX10_HOUSE, 1, message.getBool());
+		if (message.sensor == CHILD_X10_1+5) 
+         X10_transmit( MyX10_HOUSE, 5, message.getBool());
    
-    if (message.sensor == CHILD_X10_1+6) 
-       X10_transmit( MyX10_HOUSE, 2, message.getBool());
+		if (message.sensor == CHILD_X10_1+6) 
+			X10_transmit( MyX10_HOUSE, 6, message.getBool());
        
-    if (message.sensor == CHILD_X10_1+7) 
-       X10_transmit( MyX10_HOUSE, 3, message.getBool());
+		if (message.sensor == CHILD_X10_1+7) 
+			X10_transmit( MyX10_HOUSE, 7, message.getBool());
+			
+		if (message.sensor == CHILD_X10_1+8) 
+			X10_transmit( MyX10_HOUSE, 8, message.getBool());
+			
+		if (message.sensor == CHILD_X10_1+9) 
+			X10_transmit( MyX10_HOUSE, 9, message.getBool());
+			
+		if (message.sensor == CHILD_X10_1+10) 
+			X10_transmit( MyX10_HOUSE, 10, message.getBool());
+		
+		if (message.sensor == CHILD_X10_1+11) 
+			X10_transmit( MyX10_HOUSE, 11, message.getBool());
 	}
 }
 
